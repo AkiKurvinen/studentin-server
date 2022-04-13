b303ec77a641bde212c1944f16217449be79612c
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2022-04-13 13:54:29 +0300

    env fixes

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 import pg from 'pg';
+import 'dotenv/config';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
